cf60b3fe366911cd8c5521fb15b5098c5e3da7d9
plademan@cloudentity.com <plademan@cloudentity.com>
2023-03-07 13:53:13 -0500

    Changed ErrorSession background to blue

--- ce-samples-post-authn-ui-react/src/errors/ErrorSession.js
+++ ce-samples-post-authn-ui-react/src/errors/ErrorSession.js
@@ -9,7 +9,7 @@ export default function ErrorSession(props) {
       <>
         {!props.loading && props?.error != '' &&
           <>
-          <div className="card error">
+          <div className="card message">
             <h3>You are not in the system</h3>
             <p>Please ask your supervisor to add you.</p>
           </div>
